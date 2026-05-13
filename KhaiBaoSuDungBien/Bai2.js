249552d8c0e7d24fa52ebd351f0d9fd459faa163
Nguyen Ngoc Hung <nghung2k4@gmail.com>
2026-05-13 15:46:42 +0700

    Su dung bien

---
+++ KhaiBaoSuDungBien/Bai2.js
@@ -0,0 +1,4 @@
+let width = 5;
+let height = 4;
+
+document.write("Diện tích hình chữ nhật là: ", width * height);
---
+++ KhaiBaoSuDungBien/Bai3.js
@@ -0,0 +1,4 @@
+let a = Number(prompt("Nhập a: "));
+let b = Number(prompt("Nhập b: "));
+
+let test = a % b === 0 ? alert(`${a} là bội số của ${b}`) : alert(`${a} không là bội số của ${b}`);

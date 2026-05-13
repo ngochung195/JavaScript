9fa7011893bbe3fd3958383e3b526a36b7b4dbab
Nguyen Ngoc Hung <nghung2k4@gmail.com>
2026-05-13 16:02:12 +0700

    Toan tu

---
+++ SuDungToanTu/app.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+document.write("Diện tích hình chữ nhật: ", width * height);
